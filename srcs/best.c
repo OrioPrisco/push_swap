fb3f3b9fb7b1ce1277475876e5fcfaa2a71266da
OrioPrisco <47635210+OrioPrisco@users.noreply.github.com>
2023-03-03 19:23:41 +0100

    best_strat takes more factors into account
    It now cancels, translates and merges to get a better score
    for each strategy.
    This will be very nice for when ops are done in parallel.
    Considering sortedness of the produced slices could also help

--- srcs/best.c
+++ srcs/best.c
@@ -17,6 +17,42 @@
 #include <stdlib.h>
 #include "libft.h"
 
+//TODO : consider sortedness
+bool	score(t_sub_stacks *slice, size_t *result)
+{
+	t_sub_stacks	copies;
+	t_vector		ops;
+
+	cancel_ops(slice->cur.ops, 1);
+	cancel_ops(slice->other.ops, 1);
+	vector_init(&ops);
+	if (copy_sub_stack(&copies.cur, &slice->cur)
+		|| copy_sub_stack(&copies.other, &slice->other)
+		|| translate_stack_ops(&copies.cur, &copies.other, &ops)
+		|| (cancel_ops(&ops, 0), merge_ops(&ops), 0))
+		return (destroy_sub_stacks(&copies), vector_clear(&ops), 1);
+	*result = ops.size - (slice->other.ops->size - copies.other.ops->size);
+	return (destroy_sub_stacks(&copies), vector_clear(&ops), 0);
+}
+
+bool	keep_best(t_sub_stacks *current, t_sub_stacks **best)
+{
+	size_t	scores[2];
+
+	cancel_ops(current->cur.ops, 1);
+	cancel_ops((*best)->cur.ops, 1);
+	if (score(current, &scores[0]) || score(*best, &scores[1]))
+		return (1);
+	if (scores[0] < scores[1])
+	{
+		free(destroy_sub_stacks(*best));
+		*best = current;
+	}
+	else
+		free(destroy_sub_stacks(current));
+	return (0);
+}
+
 t_sub_stacks	*try_strat(const t_sub_stack *cur, const t_sub_stack *other,
 	t_f_triable *strat, void *params)
 {
@@ -37,7 +73,6 @@ t_sub_stacks	*try_strat(const t_sub_stack *cur, const t_sub_stack *other,
 	return (copies);
 }
 
-//TODO : translate to get a better idea of which is best
 t_sub_stacks	*best_strat(const t_sub_stack *cur, const t_sub_stack *other,
 	const t_funcs *strats, void *param)
 {
@@ -56,13 +91,9 @@ t_sub_stacks	*best_strat(const t_sub_stack *cur, const t_sub_stack *other,
 		current = try_strat(cur, other, strats->strats[i], param);
 		if (!current)
 			return (free(destroy_sub_stacks(best)), NULL);
-		if (current->cur.ops->size < best->cur.ops->size)
-		{
-			free(destroy_sub_stacks(best));
-			best = current;
-		}
-		else
-			free(destroy_sub_stacks(current));
+		if (keep_best(current, &best))
+			return (free(destroy_sub_stacks(best)),
+				free(destroy_sub_stacks(current)), NULL);
 		i++;
 	}
 	return (best);

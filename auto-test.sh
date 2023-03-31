dcd5abf11b833959d8c291990ca8c8044936880e
OrioPrisco <47635210+OrioPrisco@users.noreply.github.com>
2023-03-31 16:44:53 +0200

    auto-test : add valgrind error check

--- auto-test.sh
+++ auto-test.sh
@@ -9,9 +9,13 @@ do \
 	if [ $? -ne 0 ]; then
 		exit 1;
 	fi;
-	valgrind ./push_swap $ARG 2>&1 > /dev/null | grep "no leaks are possible"
+	RESULT=$(valgrind ./push_swap $ARG 2>&1 > /dev/null)
+	echo "$RESULT" | grep "no leaks are possible"
 	if [ $? -ne 0 ]; then
-		echo "leaks !";
+		exit 1;
+	fi;
+	echo "$RESULT" | grep "0 errors"
+	if [ $? -ne 0 ]; then
 		exit 1;
 	fi;
 done

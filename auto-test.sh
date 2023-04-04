27dbe0a2eb638014434a9940314c1288efc595c0
OrioPrisco <47635210+OrioPrisco@users.noreply.github.com>
2023-04-04 17:36:54 +0200

    Autotest : test own checker too
    this currently only checks if it says ok on an known good sequence of instructions, more tests are required

--- auto-test.sh
+++ auto-test.sh
@@ -2,20 +2,33 @@
 
 while :;
 do \
-	ARG=$(seq $1 $2 | shuf | tr '\n' ' ' | sed -r 's/.$//');
-	RESULT=$(echo $ARG && ./push_swap $ARG | ./checker_linux $ARG)
-	echo $RESULT;
-	echo "$RESULT" | grep "OK"
+	ARG=$(seq $1 $2 | shuf | tr '\n' ' ' | sed -r 's/.$//')
+	RESULT=$(./push_swap $ARG | ./checker_linux $ARG)
+	echo "$RESULT" | grep "OK" > /dev/null
 	if [ $? -ne 0 ]; then
+		echo "linux checker gave an error for the following args : $ARG"
+		echo "$RESULT"
 		exit 1;
 	fi;
 	RESULT=$(valgrind ./push_swap $ARG 2>&1 > /dev/null)
-	echo "$RESULT" | grep "no leaks are possible"
+	echo "$RESULT" | grep "no leaks are possible" > /dev/null
 	if [ $? -ne 0 ]; then
+		echo "valgrind shows leaks for the following args : $ARG"
+		echo "$RESULT"
 		exit 1;
 	fi;
-	echo "$RESULT" | grep "0 errors"
+	echo "$RESULT" | grep "0 errors" > /dev/null
 	if [ $? -ne 0 ]; then
+		echo "valgrind shows errors for the following args : $ARG"
+		echo "$RESULT"
 		exit 1;
 	fi;
+	RESULT=$(./push_swap $ARG | ./checker $ARG)
+	echo "$RESULT" | grep "OK" > /dev/null
+	if [ $? -ne 0 ]; then
+		echo "own checker gave an error for the following args : $ARG"
+		echo "$RESULT"
+		exit 1;
+	fi;
+	echo "test passed"
 done
